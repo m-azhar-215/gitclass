d129bde1591ae6ea76cfd8fb6b26a7ed9d76eba9
“Azhar” <azhar215@gmail.com>
2017-11-19 16:02:23 +0500

    moving

---
+++ blog.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<? include("includes/inc_head.php"); ?>
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            	<? include("includes/inc_top_header.php"); ?>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+           <!-- BOB:  Header -->
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  		<div class="carousel-inner" role="listbox">
+    
+		    <div class="item active">
+			      <img src="img/partner.jpg" alt="...">
+			      <div class="carousel-caption">
+			        <!--div class="msg-ceo">
+			           "Inabia is founded on a culture of helping people; a world where everyone is family."
+				        </div-->
+			        <!--div class="ceo">Become a Partner</div-->
+			      </div>
+		    </div>
+	   </div>
+  </div>
+  
+  
+  
+  
+  <!-- Portfolio Grid Section -->
+    <section id="partnedec">
+                   
+        <div class="container container_bg_color">
+        	<h1 style="margin-left:-12px !important; text-align:left; width:41%">Blog</h1>
+            <br>
+            <p class="inner_short_text">
+            	Coming Soon
+            </p>
+             
+    </section>
+    
+<section id="social">
+	<? include("includes/inc_social_media.php"); ?>
+</section>
+
+    <!-- Footer -->
+    <footer class="text-center">
+		<? include("includes/inc_footer.php"); ?>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+   
+    
+    
+  <? include("includes/inc_footer_script.php"); ?>  
+    
+
+  
+
+</body>
+
+</html>
---
+++ faq.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<? include("includes/inc_head.php"); ?>
+</head>
+
+<body id="page-top" class="index">
+
+    <!-- Navigation -->
+    <!--nav class="navbar navbar-default navbar-fixed-top"-->
+    <nav class="navbar navbar-default" style="background-color:#FFF">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            	<? include("includes/inc_top_header.php"); ?>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+           <!-- BOB:  Header -->
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  				<div class="carousel-inner" role="listbox" style="height:140px">
+    
+		    <div class="item active">
+			      <img src="img/partner.jpg" alt="...">
+			      <div class="carousel-caption">
+			        <!--div class="msg-ceo">
+			           "Inabia is founded on a culture of helping people; a world where everyone is family."
+				        </div-->
+			        <!--div class="ceo">Become a Partner</div-->
+			      </div>
+		    </div>
+	   </div>
+  </div>
+  
+  
+    <div style="clear:both"></div>
+  
+  <!-- Portfolio Grid Section -->
+    <section id="partnedec">
+                   
+        <div class="container container_bg_color">
+        	<br>
+         <h3 style="text-align:left">FAQs</h3> <br>
+           
+            
+            <div class="col-md-12" style="padding:0;">
+            	
+             	
+				Q: <b>What is Inabia? How can it help us?</b>
+				
+                <p  style="margin-top:8px">
+               Inabia is a consulting and staffing firm established in 2006. We have been providing powerful IT and business solutions to individuals and businesses all over the world. We believe that every individual and business has untapped potential waiting to be explored. Our vision stems from the belief that we can help them realize their potential, and in the process assist companies and individuals find their perfect match, and this is what we aim to achieve by working with you - as an employer or a partner. 
+				</p>
+				
+				<br>
+				
+				Q: <b>What is Inabia's recruitment process?</b>
+				
+                <p  style="margin-top:8px">
+               We search, pre-screen and recruit the right candidate. With our quick turn-over rate, we make sure our partners and resource are a happy fit for each other. We follow a simple five step staffing process: <br><br>
+<b>Requirement: </b>We spend quality time understanding your business, culture and requirements.<br>
+<b>Sourcing:</b> Our talented recruiters use their skills and industry knowledge to identify suitable candidates for you.<br>
+<b>Screening:</b> We invest time in getting to know the candidates; going beyond education and work experience.<br>
+<b>Selection:</b> We make sure every document is in order and requirements are met before we hand over the candidate to you. We leave no stone unturned.<br>
+<b>Monitoring:</b> We will follow up with you and make sure the candidate is performing well. We also take feedback on our services so that we can serve you better. 
+ 
+				</p>
+                
+                <br>
+				
+				Q: <b>What industries do you work with?</b>
+				
+                <p  style="margin-top:8px">
+               We started working with leading Telecom industries, and in the past few years we have expanded to other industries as well. We have the expertise to provide services regardless of the industry.  
+				</p>
+				
+				 <br>
+				
+				Q: <b>Do you provide onsite, near and off-site consultants?</b>
+				
+                <p  style="margin-top:8px">
+               We provide consulting solutions based on the project and the client's need. 
+				</p>
+				
+				 <br>
+				
+				Q: <b>How do I know if Inabia is a good match for my business?</b>
+				
+                <p  style="margin-top:8px">
+               We are happy to sit down with you and come up with a project plan that will help achieve your goals. We at Inabia believe that our reputation depends on the relationships we build with our clients and through our dedication to focus on their needs and having a positive impact on their business alike.  
+				</p>
+				
+				<br>
+				
+				Q: <b>Do you work with clients outside of the Greater Seattle Area?</b>
+				
+                <p  style="margin-top:8px">
+               Yes. Inabia headquarters is in Redmond, WA, but we have clients all across America.   
+				</p>
+				
+				<br>
+				
+				Q: <b>How quickly can I initiate a project?</b>
+				
+                <p  style="margin-top:8px">
+               This depends on the project and the expertise required. In general, and for most standard requests, we can have a consultant assigned to your project immediately. Requests that require a specific consultant or a fairly specialized skill set may have to be aligned with the specific consultant’s schedule.   
+				</p>
+				
+            </div>
+            
+                        
+            
+            <div style="height:40px; clear:both">&nbsp;</div>
+        </div>   
+                   
+      
+     
+    </section>
+    
+
+    <!-- Footer -->
+    <footer class="text-center">
+		<? include("includes/inc_footer.php"); ?>
+    </footer>
+
+    <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
+    <div class="scroll-top page-scroll visible-xs visible-sm">
+        <a class="btn btn-primary" href="#page-top">
+            <i class="fa fa-chevron-up"></i>
+        </a>
+    </div>
+
+   
+    
+    
+  <? include("includes/inc_footer_script.php"); ?>  
+    
+
+  
+
+</body>
+
+</html>
